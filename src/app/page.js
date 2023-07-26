bffdd1ee33b95e171a240bb520adb52ad43c5833
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-07-26 15:02:28 -0400

    atribución

--- src/app/page.js
+++ src/app/page.js
@@ -16,6 +16,7 @@ export default async function Home() {
     <main className="max-w-lg m-auto mt-8 p-4 flex flex-col gap-8 items-center">
       <h1 className="text-3xl font-bold text-center">Frontend Mentor Challenges</h1>
       <HomePageClient challenges={challenges} />
+      <span className="text-xs">Desafío por <a href="https://www.youtube.com/@goncypozzo" target="_blank" rel="noopener noreferrer" className="text-blue-400 underline ">Goncy</a>. Codeado por Esteban Montecinos</span>
     </main>
   );
 }
