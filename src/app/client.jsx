f609aa23e0eade3a98da1e1a1595b5b40432cd27
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-09-20 22:19:35 -0300

    size img

--- src/app/client.jsx
+++ src/app/client.jsx
@@ -71,7 +71,10 @@ export default function HomePageClient({ challenges: initialState }) {
               <img
                 src={challenge.heroImage}
                 alt={challenge.title}
-                className="object-cover w-full rounded-t-3xl h-80"
+                className="object-cover rounded-t-3xl"
+                height="320px"
+                width={480}
+                sizes="50vw"
               />
               <div className="flex flex-col gap-2 p-4">
                 <h2 className="text-xl font-medium line-clamp-1">
